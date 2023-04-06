0e5db12bd47f85ecc6d9463229069d46ef733194
git-asia <tuszynska.joanna@gmail.com>
2023-04-06 23:18:26 +0200

    Added main-page Links to logo and app name. Added smart buttons. Styled header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 import React, {SyntheticEvent, useContext, useState} from 'react';
 import {SearchContext} from "../../contex/search.context";
+import {Link} from "react-router-dom";
 
 import './Header.scss';
 import logo200 from "../images/logo200.png";
+import {Button} from "../common/Button/Button";
 
 
 export const Header = () => {
@@ -17,16 +19,20 @@ export const Header = () => {
     return (
         <header>
             <div className="head-wrapper">
-                <div className="logo-wrapper">
+                <Link className="link" to="/">
                     <img className="logo" src={logo200} alt="icon" />
-                    <h1>Adventure Tracker</h1>
+                <h1 className="title">Adventure Tracker</h1>
+                </Link>
+
+                <div className="search-add-btn">
+                    <form onSubmit={setSearchFromLocalState}>
+                        <input type="text" placeholder="Wyszukaj" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
+                        <button className="btn-search">Wyszukaj</button>
+                    </form>
+                    <Button to="/add" text="Dodaj post"/>
                 </div>
 
-                <form onSubmit={setSearchFromLocalState}>
-                    <input type="text" placeholder="Wyszukaj" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
-                    <button className="btn-search">Wyszukaj</button>
-                </form>
-                {/*<Btn to="/add" text="Dodaj ogłoszenie"/>*/}
+
             </div>
         </header>
     );
