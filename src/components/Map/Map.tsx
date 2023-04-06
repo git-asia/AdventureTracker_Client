adab55162fed7a1f0dffa10a71b46ccbfb8fb8d5
git-asia <tuszynska.joanna@gmail.com>
2023-04-06 15:52:30 +0200

    Map - added search posts and mapping posts into Markers.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,27 +1,49 @@
 import React, {useContext, useEffect, useState} from 'react';
+import {SearchContext} from "../../contex/search.context";
+import {SimplePostEntity} from "types";
+import {SinglePost} from "./SinglePost";
+import {apiUrl} from "../../config/api";
+
+import './Map.scss';
+import 'leaflet/dist/leaflet.css';
 import '../../utils/fix-map-icon'
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 
-import './Map.css';
-import 'leaflet/dist/leaflet.css';
-
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+    const [posts, setPosts] = useState<SimplePostEntity[]>([]);
+
+    useEffect(() => {
+
+        (async () => {
+
+            const res = await fetch(`${apiUrl}/post/search/${search}`, {
+                method: 'GET'
+        });
+            const data = await res.json();
+
+            setPosts(data);
+
+        })();
+    }, [search]);
 
     return (
         <div className='map'>
+            <h1>Search for: {search}</h1>
             <MapContainer center={[51.0451318,20.176074]} zoom={13} >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
                 />
-
-                <Marker position={[51.0451318,20.176074]}>
-                    <Popup>
-                        <h2>TEST 💙</h2>
-                        <p>Test pin</p>
-                    </Popup>
-                </Marker>
-
+                {
+                    posts.map((post =>
+                            <Marker key={post.id} position={[post.lat, post.lon]}>
+                                <Popup>
+                                    <SinglePost id={post.id!}/>
+                                </Popup>
+                            </Marker>
+                    ))
+                }
             </MapContainer>
         </div>
     );
