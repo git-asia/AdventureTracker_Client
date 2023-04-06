bc8104c42492f9f5330bf7f67e0df701920abfe2
git-asia <tuszynska.joanna@gmail.com>
2023-04-06 15:56:22 +0200

    App: Added Routes. Changed structure. Resigned from mui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import React from 'react';
-import logo from './logo.png';
-import './App.css';
-import {TestTypes} from "types";
+import React, {useState} from 'react';
+import {Map} from "./components/Map/Map";
+import {SearchContext} from "./contex/search.context";
+import {Routes, Route} from 'react-router-dom';
+import {Header} from "./components/Header/Header";
+// import {Form} from "./components/Form/Form";
 
+export const App = () => {
+    const [search, setSearch] = useState('');
 
-function App() {
+    return (
+        <SearchContext.Provider value={{search, setSearch}}>
+            <Header/>
+            <Routes>
 
-  const foobar: TestTypes = {
-    x: 123,
-    y: 'Asia'
-  }
+                <Route path="/" element={<Map/>} />
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            </Routes>
+        </SearchContext.Provider>
+    );
+};
 
-export default App;
