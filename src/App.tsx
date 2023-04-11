d7b0800a51cf8bf691bb32f3f458be0bcd5a1f4a
git-asia <tuszynska.joanna@gmail.com>
2023-04-11 22:25:58 +0200

    Added Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import {SearchContext} from "./contex/search.context";
 import {Routes, Route} from 'react-router-dom';
 import {Header} from "./components/Header/Header";
 import MultistepForm from "./components/MultistepForm/MultistepForm";
-
+import {NotFoundView} from "./views/NotFoundView";
+import {SinglePostView} from "./views/SinglePostView";
+import {PostsList} from "./views/PostsListView";
 
 
 export const App = () => {
@@ -16,6 +18,9 @@ export const App = () => {
             <Routes>
                 <Route path="/" element={<Map/>} />
                 <Route path="/form" element={<MultistepForm/>}/>
+                <Route path="/post/:idOfPost" element={<SinglePostView/>}/>
+                <Route path="/post" element={<PostsList/>}/>
+                <Route path="*" element={<NotFoundView/>}/>
             </Routes>
         </SearchContext.Provider>
     );
