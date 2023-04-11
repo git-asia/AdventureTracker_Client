40d26d1eb857969ec1765c50617a7fd9f314d245
git-asia <tuszynska.joanna@gmail.com>
2023-04-11 14:36:56 +0200

    Added and styled 'No found' view.

--- src/views/NotFoundView.tsx
+++ src/views/NotFoundView.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import {Button} from "../components/common/Button/Button";
+
+import './NotFoundView.scss'
 
 export const NotFoundView = () => {
-
+    return (
+    <div className="error-info">
+        <h2>Błędny adres 🛟</h2>
+        <br/> <br/>
+        <Button to="/" text="Wróć na stronę główną"/>
+    </div>
+        )
 };
