d4ed0afac3143943041076b2ddde9a0729c883ce
Joanna <tuszynska.joanna@gmail.com>
2023-06-15 17:19:38 +0200

    Readjusted code to work on localhost.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,2 @@
-export const apiUrl = process.env.REACT_APP_API_URL ?? 'http://localhost:3300/app';
+// On deploy add folder name hera, e.g 'http://localhost:3300/app'
+export const apiUrl = process.env.REACT_APP_API_URL ?? 'http://localhost:3300';
